8e2e4911713a89453731e2d3faf9fa8b916e0b4c
Abel Dieterich <dieterich.abel@gmail.com>
2020-09-18 09:41:02 +0200

    Renamed the Game script file

---
+++ Pong_Assignment/Pong_Game/PongGame.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace Pong_Game
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class PongGame : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        private const int playerSpeed = 2;
+        private const int ballSpeed = 5;
+
+        public PongGame()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here 
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            Rectangle screenSize = GraphicsDevice.Viewport.Bounds;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.S))
+            {
+                // playerOne.Y += playerSpeed;
+            }
+            
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+            {
+                // playerOne.Y -= playerSpeed;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                // playerTwo.Y += playerSpeed;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                // playerTwo.Y -= playerSpeed;
+            }
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            // Draw here
+
+            base.Draw(gameTime);
+        }
+    }
+}
--- Pong_Assignment/Pong_Game/Program.cs
+++ Pong_Assignment/Pong_Game/Program.cs
@@ -14,7 +14,7 @@ namespace Pong_Game
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new PongGame())
                 game.Run();
         }
     }
