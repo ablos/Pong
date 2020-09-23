107c5ddb25c873115dc7116a45d6298162234e06
Abel Dieterich <dieterich.abel@gmail.com>
2020-09-23 16:37:24 +0200

    Fixed merge

--- Pong_Assignment/Pong_Game/PongGame.cs
+++ Pong_Assignment/Pong_Game/PongGame.cs
@@ -23,7 +23,7 @@ namespace Pong_Game
         SpriteBatch spriteBatch;                                // Variable to store the MonoGame Sprite Batch
 
         public static bool fourPlayers = false;                 // Variable to determine if game should be played with four or two players
-        public Player[] players;                                // Array to store all players created
+        public static Player[] players;                         // Array to store all players created
         public Ball ball;                                       // Variable to store ball in
 
         public static Texture2D lifeTexture;                    // Variable to store life texture in
@@ -246,7 +246,42 @@ namespace Pong_Game
             if (location.Y > (gDevice.Viewport.Height - size.Y) || location.Y < 0)
                 direction.Y *= -1;
 
+            Rectangle ballRect = new Rectangle((int)location.X, (int)location.Y, size.X, size.Y);
+            foreach (Player p in PongGame.players)
+            {
+                Rectangle playerRect = new Rectangle(p.location.X, p.location.Y, p.size.X, p.size.Y);
 
+                if (playerRect.Intersects(ballRect))
+                {
+                    // direction x
+                    direction.X *= -1;
+
+                    if (((int)location.Y + size.Y / 2) >= (p.location.Y + p.size.Y / 5))
+                    {
+                        direction.Y = -1;
+                    }
+                    else if ((p.location.Y + p.size.Y / 5) < ((int)location.Y + size.Y / 2)
+                        && ((int)location.Y + size.Y / 2) >= 2 * (p.location.Y + p.size.Y / 5))
+                    {
+                        direction.Y = -0.5f;
+                    }
+                    else if (2 * (p.location.Y + p.size.Y / 5) < ((int)location.Y + size.Y / 2)
+                        && ((int)location.Y + size.Y / 2) >= 3 * (p.location.Y + p.size.Y / 5))
+                    {
+                        direction.Y = 0;
+                    }
+                    else if (3 * (p.location.Y + p.size.Y / 5) < ((int)location.Y + size.Y / 2)
+                        && ((int)location.Y + size.Y / 2) >= 4 * (p.location.Y + p.size.Y / 5))
+                    {
+                        direction.Y = 0.5f;
+                    }
+                    else if (4 * (p.location.Y + p.size.Y / 5) < ((int)location.Y + size.Y / 2)
+                        && ((int)location.Y + size.Y / 2) >= (p.location.Y + p.size.Y / 5))
+                    {
+                        direction.Y = 1;
+                    }
+                }
+            }
         }
     }
 
