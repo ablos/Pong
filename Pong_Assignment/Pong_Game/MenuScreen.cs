f480e526a2fd5e727366669a8097a197a273da2d
Finn Barends <f.barends@students.uu.nl>
2020-09-27 15:02:30 +0200

    menuscreen buttonsize fixes

--- Pong_Assignment/Pong_Game/MenuScreen.cs
+++ Pong_Assignment/Pong_Game/MenuScreen.cs
@@ -17,8 +17,9 @@ namespace Pong_Game
         private Vector2 fourPlayersPosition;
         private Vector2 menuPongPosition;
 
-        private Point buttonSize = new Point(200, 80);
-        private Point menuPongSize = new Point(600, 240);
+        private Point buttonSize = new Point(150, 60);
+        private Point quitButtonSize = new Point(100, 40);
+        private Point menuPongSize = new Point(500, 200);
 
         private MouseState mouseState;
         private MouseState previousMouseState;
@@ -31,9 +32,9 @@ namespace Pong_Game
             // Make the mouse pointer visible
             PongGame.pongGame.IsMouseVisible = true;
 
-            quitPosition = new Vector2(PongGame.pongGame.ScreenSize.X / 8, 4 * (PongGame.pongGame.ScreenSize.Y / 5));
-            twoPlayersPosition = new Vector2(2 * (PongGame.pongGame.ScreenSize.X / 5) - (buttonSize.X / 2), 3 * (PongGame.pongGame.ScreenSize.Y / 4) - (buttonSize.Y / 2));
-            fourPlayersPosition = new Vector2(3 * (PongGame.pongGame.ScreenSize.X / 5) - (buttonSize.X / 2), 3 * (PongGame.pongGame.ScreenSize.Y / 4) - (buttonSize.Y / 2));
+            quitPosition = new Vector2(PongGame.pongGame.ScreenSize.X / 20, 7 * (PongGame.pongGame.ScreenSize.Y / 8));
+            twoPlayersPosition = new Vector2(2 * (PongGame.pongGame.ScreenSize.X / 5) - (buttonSize.X / 2), 2 * (PongGame.pongGame.ScreenSize.Y / 3) - (buttonSize.Y / 2));
+            fourPlayersPosition = new Vector2(3 * (PongGame.pongGame.ScreenSize.X / 5) - (buttonSize.X / 2), 2 * (PongGame.pongGame.ScreenSize.Y / 3) - (buttonSize.Y / 2));
             menuPongPosition = new Vector2((PongGame.pongGame.ScreenSize.X / 2) - (menuPongSize.X / 2), PongGame.pongGame.ScreenSize.Y / 10);
 
             CreateButtons();
@@ -41,11 +42,7 @@ namespace Pong_Game
 
         private void CreateButtons()
         {
-            Button quitButton = new Button(buttonSize, quitPosition, QuitButton, PongGame.pongGame.quitButtonTexture);
-            Button twoPlayersButton = new Button(buttonSize, twoPlayersPosition, TwoPlayersButton, PongGame.pongGame.twoPlayersButtonTexture);
-            Button fourPlayersButton = new Button(buttonSize, fourPlayersPosition, FourPlayersButton, PongGame.pongGame.fourPlayersButtonTexture);
-
-            buttons[0] = new Button(buttonSize, quitPosition, QuitButton, PongGame.pongGame.quitButtonTexture);
+            buttons[0] = new Button(quitButtonSize, quitPosition, QuitButton, PongGame.pongGame.quitButtonTexture);
             buttons[1] = new Button(buttonSize, twoPlayersPosition, TwoPlayersButton, PongGame.pongGame.twoPlayersButtonTexture);
             buttons[2] = new Button(buttonSize, fourPlayersPosition, FourPlayersButton, PongGame.pongGame.fourPlayersButtonTexture);
         }
@@ -79,7 +76,7 @@ namespace Pong_Game
         // Draw method
         public void Draw()
         {
-            PongGame.pongGame.spriteBatch.Draw(PongGame.pongGame.menuPongTexture, menuPongPosition, Color.White);
+            PongGame.pongGame.spriteBatch.Draw(PongGame.pongGame.menuPongTexture, new Rectangle(menuPongPosition.ToPoint(), menuPongSize), Color.White);
 
             foreach (Button b in buttons)
                 b.Draw();
